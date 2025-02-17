15cc11e16a63086b85b92373f5b5c42c3af5c917
Thanushree560 <iamthanushree.venkatesh@gmail.com>
2025-02-17 14:47:40 +0530

    my java program

---
+++ Arithmeticoperations.java
@@ -0,0 +1,15 @@
+class Arithmeticoperations
+{
+  public static void main(String args[])
+  {
+     int a,b,sum,diff,prod,div,mod;
+     a=10;
+     b=20;
+    sum=a+b;
+   diff =a-b;
+   prod=a*b;
+   div=a/b;
+   mod=a%b;
+   System.out.println("Sum="+sum+"\n Diff="+diff+"\n Prod="+prod+"\nDiv="+div+"\nMod="+mod);
+ }
+}
